cb58c54ce8218ecd3f04f786564532569694b2b4
jon.xia <xiajia@vip.qq.com>
2018-06-14 19:26:41 +0800

    get a non typescript version

---
+++ src/main.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+
+import './styles/quasar.styl'
+import 'quasar-extras/animate'
+import 'quasar-extras/roboto-font'
+import 'quasar-extras/material-icons'
+import Quasar, * as All from 'quasar'
+
+Vue.use(Quasar, {
+  components: All,
+  directives: All,
+  plugins: All
+})
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
